8c41fb97af668b2ef98018da2de0cbf1dcd779d5
Jose Tellez Anaya <116173395+Pepetl@users.noreply.github.com>
2025-01-24 18:19:43 -0600

    Managers

--- GlobalGameJam/Assets/Scripts/Conductor.cs
+++ GlobalGameJam/Assets/Scripts/Conductor.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Prueba3 : MonoBehaviour
+public class Conductor : MonoBehaviour
 {
-    // Start is called before the first frame update
+ 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
         
--- GlobalGameJam/Assets/Scripts/GameManager.cs
+++ GlobalGameJam/Assets/Scripts/GameManager.cs
@@ -1,18 +1,68 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState
+{
+    None,
+    MainMenu,
+    Playing,
+    RestartGame
+
+
+}
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager gameManager;
+
+    // Estado actual del juego
+    public static GameState gameState = GameState.None;
+
+    private void Awake()
     {
-        
+        if (gameManager != null && gameManager != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        gameManager = this;
+        gameState = GameState.None;
+
+    }
+    public void SetGameState(GameState newState)
+    {
+        if (gameState == newState)
+        {
+            Debug.LogWarning($"El juego ya est� en estado {newState}");
+            return;
+        }
+
+        gameState = newState;
+        switch (gameState)
+        {
+            case GameState.None:
+
+                break;
+            case GameState.MainMenu:
+
+                break;
+            case GameState.Playing:
+
+                break;
+            case GameState.RestartGame:
+
+                break;
+            default:
+
+                break;
+        }
+    }
+    public GameState GetGameState()
+    {
+        return gameState;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
